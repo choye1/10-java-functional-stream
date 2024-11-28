af2f621b156847fe829c1428957ca877cb38b725
choye1 <makcim233237@gmail.com>
2024-11-28 13:01:31 +0500

    task 5

---
+++ task05/src/com/example/task05/MailMessage.java
@@ -0,0 +1,30 @@
+package com.example.task05;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MailMessage implements IMail<String>{
+    private String Content;
+    private String to;
+    private String from;
+
+    public MailMessage(String from, String to, String Content) {
+        this.from = from;
+        this.to = to;
+        this.Content = Content;
+    }
+
+    public String getContent() {
+        return Content;
+    }
+
+
+    public String getTo() {
+        return to;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+}
